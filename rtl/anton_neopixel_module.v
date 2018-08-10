55e399540214f4d5850e99c1cee1522b592d1209
Anton Krug <antonkrug@gmail.com>
2018-08-10 02:08:40 +0100

    Moved registers logic inside own module

--- rtl/anton_neopixel_module.v
+++ rtl/anton_neopixel_module.v
@@ -33,83 +33,35 @@ module anton_neopixel_module (
   localparam BUFFER_BITS = `CLOG2(BUFFER_END+1);   
 
 
-  reg [7:0]  bus_data_out_buffer;
-  reg [7:0]  pixels[BUFFER_END:0];
-            
+  wire [7:0]  pixels[BUFFER_END:0];
+  wire [12:0] reg_max;
+  wire        reg_ctrl_init;
+  wire        reg_ctrl_limit;
+  wire        reg_ctrl_run;
+  wire        reg_ctrl_loop;
+  wire        reg_ctrl_32bit;
   
-  // 13 bits in total apb is using 16 bus but -2 bit are dropped for word 
-  // alignment and 1 bit used to detect control registry accesses
-  reg [12:0] reg_max; 
-  
-  reg        reg_ctrl_init  = 'b0;
-  reg        reg_ctrl_limit = 'b0; // Change this only when the pixel data are not streamed
-  reg        reg_ctrl_run   = 'b0;
-  reg        reg_ctrl_loop  = 'b0;
-  reg        reg_ctrl_32bit = 'b0; // Change this only when the pixel data are not streamed
 
-  
-  // TODO: detect verilator and use it only there
-  // for simulation to track few cycles of the whole process to make sure after 
-  // reset nothing funny is happening
-  
-  always @(posedge busClk) begin
-    if (reg_ctrl_init) begin
-      reg_ctrl_init   <= 'b0;
-      reg_ctrl_limit  <= 'b0;
-      reg_ctrl_run    <= 'b0;
-      reg_ctrl_loop   <= 'b0;
-      reg_ctrl_32bit  <= 'b0;
-    end else begin
-
-      if (busWrite) begin
-        if (busAddr[13] == 'b0) begin
-
-          // Write buffer
-          pixels[busAddr[BUFFER_BITS-1:0]] <= busDataIn;
-        end else begin
-
-          // Write register
-          case (busAddr[1:0])
-            0: reg_max[7:0]  <= busDataIn;
-            1: reg_max[12:8] <= busDataIn[4:0];
-            2: {reg_ctrl_32bit, reg_ctrl_loop, reg_ctrl_run, reg_ctrl_limit, reg_ctrl_init} <= busDataIn[4:0];
-          endcase
-        end
-      end
-      if (busRead) begin
-        if (busAddr[13] == 'b0) begin
-          
-          // Read buffer
-          bus_data_out_buffer <= pixels[busAddr[BUFFER_BITS-1:0]];
-        end else begin
-
-          // Read register
-          case (busAddr[1:0])
-            0: bus_data_out_buffer <= reg_max[7:0];
-            1: bus_data_out_buffer <= { 3'b000, reg_max[12:8]};
-            2: bus_data_out_buffer <= {3'b000, reg_ctrl_32bit, reg_ctrl_loop, reg_ctrl_run, reg_ctrl_limit, reg_ctrl_init};
-            3: bus_data_out_buffer <= {7'b0000000, state};
-          endcase
-        end
-      end
-    end
-  end
-
-
-  always @(posedge busClk) if (stream_sync_of) reg_ctrl_run <= reg_ctrl_loop;
-
-
-  anton_neopixel_stream #(
+  anton_neopixel_registers #(
     .BUFFER_END(BUFFER_END)
-  ) stream(
+  ) registers(
+    .busClk(busClk),
+    .busAddr(busAddr),
+    .busDataIn(busDataIn),
+    .busWrite(busWrite),
+    .busRead(busRead),
+    .busDataOut(busDataOut),
+
+    .stream_sync_of(stream_sync_of),
+
+    .state(neoState),
     .pixels(pixels),
-    .state(state),
-    .pixel_index(pixel_index),
-    .pixel_bit_index(pixel_bit_index),
-    .bit_pattern_index(bit_pattern_index),
-    .reg_ctrl_32bit(reg_ctrl_32bit),
+    .reg_max(reg_max),
+    .reg_ctrl_init(reg_ctrl_init),
+    .reg_ctrl_limit(reg_ctrl_limit),
     .reg_ctrl_run(reg_ctrl_run),
-    .neoData(neoData)
+    .reg_ctrl_loop(reg_ctrl_loop),
+    .reg_ctrl_32bit(reg_ctrl_32bit)
   );
 
 
@@ -117,12 +69,25 @@ module anton_neopixel_module (
   wire [4:0] pixel_bit_index;
   wire [BUFFER_BITS-1:0] pixel_index;
   wire [BUFFER_BITS-1:0] pixel_index_max;
-  wire state;
   wire stream_output;
   wire stream_reset;
   wire stream_bit_of;
   wire stream_pixel_of;
   wire stream_sync_of;
+
+
+  anton_neopixel_stream #(
+    .BUFFER_END(BUFFER_END)
+  ) stream(
+    .pixels(pixels),
+    .state(neoState),
+    .pixel_index(pixel_index),
+    .pixel_bit_index(pixel_bit_index),
+    .bit_pattern_index(bit_pattern_index),
+    .reg_ctrl_32bit(reg_ctrl_32bit),
+    .reg_ctrl_run(reg_ctrl_run),
+    .neoData(neoData)
+  );
 
 
   anton_neopixel_stream_logic #(
@@ -141,7 +106,7 @@ module anton_neopixel_module (
     .pixel_bit_index(pixel_bit_index),
     .pixel_index(pixel_index),
     .pixel_index_max(pixel_index_max),
-    .state(state),
+    .state(neoState),
     .stream_output(stream_output),
     .stream_reset(stream_reset),
     .stream_bit_of(stream_bit_of),
@@ -149,8 +114,5 @@ module anton_neopixel_module (
     .stream_sync_of(stream_sync_of)
   );
 
-
-  assign busDataOut = bus_data_out_buffer;
-  assign neoState   = state;
   
 endmodule
---
+++ rtl/anton_neopixel_registers.v
@@ -0,0 +1,92 @@
+`include "anton_common.vh"
+
+module anton_neopixel_registers (
+  input         busClk,
+  input  [13:0] busAddr,
+  input  [7:0]  busDataIn,
+  input         busWrite,
+  input         busRead,
+  output [7:0]  busDataOut,
+
+  input         stream_sync_of,
+
+  input         state,
+  output [7:0]  pixels[BUFFER_END:0],
+  output [12:0] reg_max,
+  output        reg_ctrl_init,
+  output        reg_ctrl_limit,
+  output        reg_ctrl_run,
+  output        reg_ctrl_loop,
+  output        reg_ctrl_32bit
+);
+
+  reg [7:0]  pixels[BUFFER_END:0];
+  reg [7:0]  busDataOut;
+
+  // 13 bits in total apb is using 16 bus but -2 bit are dropped for word 
+  // alignment and 1 bit used to detect control registry accesses
+  reg [12:0] reg_max; 
+  
+  reg        reg_ctrl_init  = 'b0;
+  reg        reg_ctrl_limit = 'b0; // Change this only when the pixel data are not streamed
+  reg        reg_ctrl_run   = 'b0;
+  reg        reg_ctrl_loop  = 'b0;
+  reg        reg_ctrl_32bit = 'b0; // Change this only when the pixel data are not streamed
+
+  parameter  BUFFER_END  = `BUFFER_END_DEFAULT;   // read anton_common.vh
+  localparam BUFFER_BITS = `CLOG2(BUFFER_END+1);  // minimum required amount of bits to store the BUFFER_END
+
+
+  // TODO: detect verilator and use it only there
+  // for simulation to track few cycles of the whole process to make sure after 
+  // reset nothing funny is happening
+  
+
+  always @(posedge busClk) begin
+    if (reg_ctrl_init) begin
+      reg_ctrl_init   <= 'b0;
+      reg_ctrl_limit  <= 'b0;
+      reg_ctrl_run    <= 'b0;
+      reg_ctrl_loop   <= 'b0;
+      reg_ctrl_32bit  <= 'b0;
+    end else begin
+
+      if (busWrite) begin
+        if (busAddr[13] == 'b0) begin
+
+          // Write buffer
+          pixels[busAddr[BUFFER_BITS-1:0]] <= busDataIn;
+        end else begin
+
+          // Write register
+          case (busAddr[1:0])
+            0: reg_max[7:0]  <= busDataIn;
+            1: reg_max[12:8] <= busDataIn[4:0];
+            2: {reg_ctrl_32bit, reg_ctrl_loop, reg_ctrl_run, reg_ctrl_limit, reg_ctrl_init} <= busDataIn[4:0];
+          endcase
+        end
+      end
+      if (busRead) begin
+        if (busAddr[13] == 'b0) begin
+          
+          // Read buffer
+          busDataOut <= pixels[busAddr[BUFFER_BITS-1:0]];
+        end else begin
+
+          // Read register
+          case (busAddr[1:0])
+            0: busDataOut <= reg_max[7:0];
+            1: busDataOut <= { 3'b000, reg_max[12:8]};
+            2: busDataOut <= {3'b000, reg_ctrl_32bit, reg_ctrl_loop, reg_ctrl_run, reg_ctrl_limit, reg_ctrl_init};
+            3: busDataOut <= {7'b0000000, state};
+          endcase
+        end
+      end
+    end
+  end
+
+
+  always @(posedge busClk) if (stream_sync_of) reg_ctrl_run <= reg_ctrl_loop;
+
+
+endmodule
