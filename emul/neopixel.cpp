5403af05c58b6718e11738d5c0f4f15be322d87c
Anton Krug <antonkrug@gmail.com>
2018-09-04 00:20:12 +0100

    separated buffer population. using lcov export command

--- emul/neopixel.cpp
+++ emul/neopixel.cpp
@@ -21,6 +21,7 @@ Vanton_neopixel_apb_top *uut;
 VerilatedVcdC *tfp;
 vluint64_t sim_time;
 
+
 double sc_time_stamp () {
   return sim_time*50;
 }
@@ -35,6 +36,19 @@ void cycleClocks() {
   uut->clk7mhz = uut->clk7mhz ? 0 : 1;
   uut->eval();
   tfp->dump(sim_time += 25);
+}
+
+
+void populatePixelBuffer(NeoPixelDriver *driver) {
+  neopixelWriteApbByte(0, 0xff);
+  neopixelWriteApbByte(4, 0x02);
+  neopixelWriteApbByte(8, 0x18);
+  neopixelWriteApbByte(12, 0xDE); // this shouldn't get displayed in 32bit mode
+  neopixelWriteApbByte(16, 0xCE);
+  neopixelWriteApbByte(20, 0xAD);
+  neopixelWriteApbByte(24, 0x98);
+  neopixelWriteApbByte(28, 0x01); // this shouldn't get displayed in 32bit mode
+  neopixelWriteApbByte(32, 0x00);
 }
 
 
@@ -58,16 +72,7 @@ int main(int argc, char** argv) {
   uut->anton_neopixel_apb_top__DOT__test_unit = 0;
 
   NeoPixelDriver *driver = new NeoPixelDriver(0, 60);
-
-  neopixelWriteApbByte(0,  0xff);
-  neopixelWriteApbByte(4,  0x02);
-  neopixelWriteApbByte(8,  0x18);
-  neopixelWriteApbByte(12, 0xDE); // this shouldn't get displayed in 32bit mode
-  neopixelWriteApbByte(16, 0xCE);
-  neopixelWriteApbByte(20, 0xAD);
-  neopixelWriteApbByte(24, 0x98);
-  neopixelWriteApbByte(28, 0x01); // this shouldn't get displayed in 32bit mode
-  neopixelWriteApbByte(32, 0x00);
+  populatePixelBuffer(driver);
 
   driver->writeRegisterMax(0xface);
   driver->writeRegisterMax(7);
@@ -76,8 +81,9 @@ int main(int argc, char** argv) {
   driver->writeRegisterCtrl(CTRL_RUN | CTRL_32 | CTRL_LIMIT);
 
   // test 1 run 32bit - soft limit mode with 7bytes max -> 8 bytes size (which is 2 pixels in 32bit mode)
-  while (driver->testRegisterCtrl(CTRL_RUN))
-    cycleClocks(); // Wait for the cycle to finish
+  while (driver->testRegisterCtrl(CTRL_RUN)) { // Wait for the cycle to finish
+    cycleClocks(); 
+  }
 
   cycleClocks();
   cycleClocks();
@@ -104,7 +110,7 @@ int main(int argc, char** argv) {
   uut->final();
 
   #if VM_COVERAGE
-    VerilatedCov::write("lcov.info");
+    VerilatedCov::writeLcov("lcov.info");
   #endif
 
   tfp->close();
