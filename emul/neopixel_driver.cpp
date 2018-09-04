70ecfb65fe276f7132b2f0650ce5522e332eca0a
Anton Krug <antonkrug@gmail.com>
2018-09-04 01:24:36 +0100

    Read/Write pixel buffer from the driver

--- emul/neopixel_driver.cpp
+++ emul/neopixel_driver.cpp
@@ -7,32 +7,40 @@ NeoPixelDriver::NeoPixelDriver(uint32_t base, uint32_t pixels) {
 }
 
 void NeoPixelDriver::writeRegister(uint16_t addr, uint8_t data) {
-  neopixelWriteApbByte(addr | 1 << 15, data);
+  neopixelWriteApbByte(addr << 2 | NEOPIXEL_CTRL_BIT, data);
 }
 
 uint8_t NeoPixelDriver::readRegister(uint16_t addr) {
-  return (neopixelReadApbByte(addr | 1 << 15));
+  return (neopixelReadApbByte(addr << 2 | NEOPIXEL_CTRL_BIT));
+}
+
+void NeoPixelDriver::writePixelByte(uint16_t pixel, uint8_t value) {
+  neopixelWriteApbByte(pixel << 2 & NEOPIXEL_CTRL_BIT_MASK, value);
+}
+
+uint8_t NeoPixelDriver::readPixelByte(uint16_t addr) {
+  return (neopixelReadApbByte(addr << 2 & NEOPIXEL_CTRL_BIT_MASK));
 }
 
 // TODO: use enum for the offsets
 void NeoPixelDriver::writeRegisterMax(uint16_t value) {
   writeRegister(0, value & 0xFF);
-  writeRegister(4, (value >> 8) & 0xFF);
+  writeRegister(1, (value >> 8) & 0xFF);
 }
 
 void NeoPixelDriver::writeRegisterCtrl(uint8_t value) {
-  writeRegister(8, value);
+  writeRegister(2, value);
 }
 
 uint8_t NeoPixelDriver::readRegisterCtrl() {
-  return (readRegister(8));
+  return (readRegister(2));
 }
 
 void NeoPixelDriver::setPixelLength(uint16_t pixels) {
 }
 
 uint8_t NeoPixelDriver::testRegisterCtrl(uint8_t mask) {
-  return (readRegister(8) & mask);
+  return (readRegister(2) & mask);
 }
 
 void NeoPixelDriver::syncStart() {
--- emul/neopixel_driver.h
+++ emul/neopixel_driver.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+#define NEOPIXEL_CTRL_BIT       ((uint16_t)(1 << 15))
+#define NEOPIXEL_CTRL_BIT_MASK (~(uint16_t)(1 << 15))
+
 class NeoPixelDriver {
   private:
     uint32_t base;
@@ -18,6 +21,11 @@ class NeoPixelDriver {
     // void setPixel(uint32_t color);
     // void setPixel(uint8_t red, uint8_t green, uint8_t blue);
     // void setPixelRaw(uint32_t color);
+
+    // TODO: Pixel words
+    void writePixelByte(uint16_t pixel, uint8_t value);
+
+    uint8_t readPixelByte(uint16_t pixel);
 
     void writeRegisterMax(uint16_t value);
 
