82366d11585fb8c0d81b589787a214383da1f8be
Anton Krug <antonkrug@gmail.com>
2018-09-14 16:56:14 +0100

    More specific about timeouts, allows the driver to read the physical outputs

--- emul/test_helper.h
+++ emul/test_helper.h
@@ -5,8 +5,10 @@
 
 extern void testFailed();
 extern void testWait(uint32_t time);
-extern void testTimeoutStart(uint32_t timeout);
+extern void testTimeoutStart(uint32_t microSeconds);
 extern bool testTimeoutIsExpired();
+
+extern bool readNeoData();
 
 template <typename T>
 void testAssertEquals(std::string text, T expected, T actual);
--- emul/test_helper_bsp_simulation.cpp
+++ emul/test_helper_bsp_simulation.cpp
@@ -4,6 +4,7 @@
 
 #include "test_helper.h"
 #include "neopixel.h"
+#include "neopixel_simulation.h"
 
 extern void simulationDone();
 extern void cycleClocks();
@@ -20,10 +21,17 @@ void testWait(uint32_t time) {
   }
 }
 
-void testTimeoutStart(uint32_t timeout) {
-  testTimeoutTime = sim_time + timeout;
+// 3 simulation steps are quired to for 100ns in simulation to pass.
+// Each simulation step is 25units of the counter, so 75 units of the
+// counter means 100ns in time, therefore 750 counter units = 1us.
+void testTimeoutStart(uint32_t microSeconds) {
+  testTimeoutTime = sim_time + ((vluint64_t)(microSeconds) * 750);
 }
 
 bool testTimeoutIsExpired() {
   return(sim_time > testTimeoutTime);
+}
+
+bool readNeoData() {
+  return uut->neoData;
 }
