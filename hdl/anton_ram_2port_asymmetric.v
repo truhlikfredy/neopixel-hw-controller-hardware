faa02e25e521af90924270b841ee43f3aefc0be8
Anton Krug <antonkrug@gmail.com>
2019-02-11 01:24:19 +0000

    Split symmetric and asymmetric memory, allow configure depth/width

---
+++ hdl/anton_ram_2port_asymmetric.v
@@ -0,0 +1,42 @@
+`include "anton_common.vh"
+
+// Synplify inference patterns into blocks:
+// https://www.microsemi.com/document-portal/doc_view/129966-inferring-microsemi-smartfusion2-ram-blocks-app-note
+
+// Using LSRAM 18K blocks pipelined memory blocks, example #18 but changed widths
+module anton_ram_2port_asymmetric #(
+  parameter  BUFFER_END         = `BUFFER_END_DEFAULT,  // read anton_common.vh
+  parameter  BUFFER_WIDTH_READ  = 16,
+  parameter  BUFFER_WIDTH_WRITE = 8,
+  localparam BUFFER_BITS        = `CLOG2(BUFFER_END+1)  // minimum required amount of bits to store the BUFFER_END
+)(
+  input                     clk,
+
+  input  [BUFFER_BITS-1:0]  rAddr,
+  output [BUFFER_WIDTH-1:0] dOut,
+
+  input                     wr,
+  input  [BUFFER_BITS-1:0]  wAddr,
+  input  [BUFFER_WIDTH_WRITE-1:0] dIn
+);
+
+reg [BUFFER_BITS-1:0]  raddr_reg;
+reg [BUFFER_WIDTH_READ-1:0] mem [0:BUFFER_END-1];
+reg [BUFFER_WIDTH_READ-1:0] dOut;
+reg [BUFFER_WIDTH_READ-1:0] dOutB;
+
+always@ (posedge clk)
+begin
+  raddr_reg <= rAddr;
+  dOutB <= mem[raddr_reg];
+  if (wr)
+    mem[wAddr] <= dIn;
+end
+
+always@ (posedge clk)
+begin
+  dOut <= dOutB;
+end
+
+
+endmodule
